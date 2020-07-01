c60d92dc7763835665a7b1ad2a3f3b36728ac5e3
Abner Silva <link_abner.p.s@hotmail.com>
2020-06-30 22:15:34 -0300

    Updates

---
+++ precache-manifest.7656857eac4dba6ea5c4c1bbbd913f05.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c1a7f5bf35373b71679739e7b8588f",
+    "url": "/star-wars-wiki/index.html"
+  },
+  {
+    "revision": "60c7cbd5bdc31e41a72f",
+    "url": "/star-wars-wiki/static/css/2.f005c5ff.chunk.css"
+  },
+  {
+    "revision": "0ac568abd601376340e6",
+    "url": "/star-wars-wiki/static/css/main.ba8e01aa.chunk.css"
+  },
+  {
+    "revision": "60c7cbd5bdc31e41a72f",
+    "url": "/star-wars-wiki/static/js/2.a2cb940c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/star-wars-wiki/static/js/2.a2cb940c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac568abd601376340e6",
+    "url": "/star-wars-wiki/static/js/main.38932e7c.chunk.js"
+  },
+  {
+    "revision": "b72abadad035dc467323",
+    "url": "/star-wars-wiki/static/js/runtime-main.b4fa65d3.js"
+  },
+  {
+    "revision": "1e9cd186b02026e9e7543dc7a5e7fd43",
+    "url": "/star-wars-wiki/static/media/background.1e9cd186.webp"
+  },
+  {
+    "revision": "f5bdff6225b63923b571d039f42d4519",
+    "url": "/star-wars-wiki/static/media/logoYellow.f5bdff62.png"
+  },
+  {
+    "revision": "ed5ab9e580a8da57c01a7af70a900614",
+    "url": "/star-wars-wiki/static/media/nave.ed5ab9e5.png"
+  },
+  {
+    "revision": "de1364c74d35ece81e42df588fff6a20",
+    "url": "/star-wars-wiki/static/media/planeta.de1364c7.png"
+  },
+  {
+    "revision": "04ef2bb671bff96058b7a37fe3ef23b8",
+    "url": "/star-wars-wiki/static/media/stormtrooper.04ef2bb6.png"
+  }
+]);
