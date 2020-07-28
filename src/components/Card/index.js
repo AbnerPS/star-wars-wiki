63ef59bfc3848d27dadf1f2fd87b8ad31d1ff005
Abner Silva <link_abner.p.s@hotmail.com>
2020-07-27 21:04:23 -0300

    Ajustado componente Card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaEye } from 'react-icons/fa'
 import './styles.css'
 
 function Card(props) {
     return (
         <div className="card">
-            {props.conteudo}
-            <Link to={props.url}>Ver mais</Link>
+            { props.children }
+            <Link to={props.url}>See more <FaEye /></Link>
         </div>
     )
 }
